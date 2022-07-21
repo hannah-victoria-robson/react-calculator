d5137757ec61808d0f8f23ad1ff79ed44244679e
Hannah-Robson <hannah.v.robson@gmail.com>
2022-07-22 11:04:24 +1200

    add calculator buttons

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 function Calculator() {
 	return (
 		<>
-			<section className="calc-container"></section>
+			<main>
+				<section className="calc-container">
+					<article>
+						<header>Calculator</header>
+						<div>
+							<output className="output"></output>
+							<button className="AC">AC</button>
+							<button className="number-toggle">+/-</button>
+							<button className="percent">%</button>
+							<button className="times">X</button>
+							<button className="subtract">-</button>
+							<button className="add">+</button>
+							<button className="equals">=</button>
+							<button className="nine">9</button>
+							<button className="eight">8</button>
+							<button className="seven">7</button>
+							<button className="six">6</button>
+							<button className="five">5</button>
+							<button className="four">4</button>
+							<button className="three">3</button>
+							<button className="two">2</button>
+							<button className="one">1</button>
+							<button className="zero">0</button>
+							<button className="decimal">.</button>
+						</div>
+					</article>
+				</section>
+			</main>
 		</>
 	)
 }
