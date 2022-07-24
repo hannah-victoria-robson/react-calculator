408528365b85e78d141044a8d758645274b1d485
Hannah-Robson <hannah.v.robson@gmail.com>
2022-07-25 11:25:22 +1200

    center calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import Footer from './Footer'
-// import Draggable from 'react-draggable'
+import Draggable from 'react-draggable'
 // import Details from './Details'
-import React from 'react'
+import React, { useState } from 'react'
+// import ReactDOM from 'react-dom'
+
 import Calculator from './Calculator'
 
 function App() {
@@ -10,11 +12,14 @@ function App() {
 		<div className="App">
 			<header>Calculator</header>
 			<main>
-				{/* <Draggable> */}
-				<div className="box">
-					<Calculator />
+				<div className="calc-parent">
+					<>{/* <section className="calc-frame" id="border"></section> */}</>
+					<Draggable defaultPosition={{ x: -125, y: 200 }}>
+						<div className="box">
+							<Calculator id="calc" />
+						</div>
+					</Draggable>
 				</div>
-				{/* </Draggable> */}
 				{/* <Details /> */}
 			</main>
 			<Footer />{' '}
