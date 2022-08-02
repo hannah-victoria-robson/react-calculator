3adc58a50950f48fddf78bed63575ce28d0ef88d
Hannah-Robson <hannah.v.robson@gmail.com>
2022-08-03 09:40:03 +1200

    add focus and main files

--- src/Details.jsx
+++ src/Details.jsx
@@ -6,27 +6,56 @@ function Details() {
 			<section className="details-section">
 				<details className="details-item">
 					<summary>Project Details</summary>
-					<p>
-						Stack used: HTML, CSS, React, <a href="https://jestjs.io/"> Jest</a>
-						,&nbsp;
-						<a href="https://www.npmjs.com/package/react-draggable">
-							Draggable
-						</a>
-					</p>
-					<p>
-						Design: The calculator design is based off of the mac desktop
-						calculator app. This was done as a way to practice front-end styling
-						from a design brief.
-					</p>
-					<p>
-						Functionality: The functionality of this calculator is also based on
-						the mac desktop calculator app.
-					</p>
-					<p>
-						<a href="https://github.com/hannah-victoria-robson/react-calculator">
-							Project repo on Github
-						</a>
-					</p>
+					<ul>
+						<li className="details-list-item">
+							<p>
+								Stack used: HTML, CSS, React,{' '}
+								<a href="https://jestjs.io/"> Jest</a>
+								,&nbsp;
+								<a href="https://www.npmjs.com/package/react-draggable">
+									Draggable
+								</a>
+							</p>
+						</li>
+						<li className="details-list-item">
+							<p>
+								Design: The calculator design is based off of the mac desktop
+								calculator app. This was done as a way to practice front-end
+								styling from a design brief.
+							</p>
+						</li>
+						<li className="details-list-item">
+							<p>
+								Functionality: The functionality of this calculator is also
+								based on the mac desktop calculator app.
+							</p>
+						</li>
+						<li className="details-list-item">
+							<p>
+								<a href="https://github.com/hannah-victoria-robson/react-calculator">
+									Project repo on Github
+								</a>
+							</p>
+						</li>
+						<li className="details-list-item">
+							<p>
+								Main focus: Functions, testing functions using Jest. <br />
+								Files of note:
+								<ul>
+									<li>
+										<a href="https://github.com/hannah-victoria-robson/react-calculator/blob/main/src/functions.js">
+											functions.js
+										</a>{' '}
+									</li>
+									<li>
+										<a href="https://github.com/hannah-victoria-robson/react-calculator/blob/main/src/functions.test.js">
+											functions.test.js
+										</a>{' '}
+									</li>
+								</ul>
+							</p>
+						</li>
+					</ul>
 				</details>
 			</section>
 		</>
